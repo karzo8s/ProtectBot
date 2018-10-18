865bf697903f89b7b1d42a4aa476c0de7fe425f5
karzo8s <44073963+karzo8s@users.noreply.github.com>
2018-10-18 18:33:04 +0200

    Update botdiscord.js

--- botdiscord.js
+++ botdiscord.js
@@ -105,7 +105,7 @@ channel.guild.owner.send(`<@!${channelremover.id}>
 
 
 client.on('ready',async () => {
-let streaming = [`ProTection Mode : ON☝🏻 `, `Protection 24/7 ☝🏻`];
+let streaming = [`SystemBots⚙️ `, `Protection 24/7 ☝🏻`];
 client.user.setActivity(streaming[Math.floor(Math.random() * streaming.length)], {type: 1, url: "https://twitch.tv/6xlez1"});
 setInterval(() => {
 client.user.setActivity(streaming[Math.floor(Math.random() * streaming.length)], {type: 1, url: "https://twitch.tv/6xlez1"});
